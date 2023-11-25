763ac5430f886b6e468b674a18e3a4170bede883
Glimmer Rhodes <glimmerrhodes@gmail.com>
2023-11-24 19:57:53 -0800

    add .aab -> .mid output because why not

--- rb-sandbox/aab_parse.rb
+++ rb-sandbox/aab_parse.rb
@@ -1,14 +1,63 @@
 # hastily parse SND section of .aab files to grab event names
-# timed to .anm files
-# --scan to scan for event names in wwise bnk & wem files
+# these are usually paired with/timed to .anm files
+# --input <aab file | dir of aab files> to parse
+# --scan <dir> to scan for event names in wwise bnk & wem files
+# --midi <dir> to output midi files for each file with event timeline
 
-require "murmurhash3"
+USAGE = <<EOF
+
+Usage: ruby aab_parse.rb --input <FILE | DIR> [--scan <WWISE_DIR>] [--midi <MIDI_DIR>]
+
+--input <FILE | DIR> - .aab file or directory of .aab files to parse. Directories will be scanned recursively.
+--scan <WWISE_DIR> - Directory of wwise .bnk & .wem files to scan for event names
+--midi <MIDI_DIR> - Directory to output .mid files for each file with event timeline
+EOF
+
 require "./wwise-search.rb"
+require 'optparse'
+require 'fileutils'
+
+# big endian
+# msb is set if there are more bytes to read
+def midi_var_len_pack(val)
+  val = val.to_i
+  bytes = []
+  loop do
+    bytes << (val & 0x7F)
+    val >>= 7
+    break if val == 0
+  end
+  bytes = bytes.reverse.map.with_index do |b, i|
+    b |= 0x80 if i < bytes.length - 1
+    b
+  end
+  bytes.pack("C*")
+end
+
+def u24_pack_be(val)
+  bytes = [val.to_i].pack("N")[1..-1]
+end
+
+options = {}
+OptionParser.new do |opt|
+  opt.on('-i', '--input <FILE | DIR>') { |o| options[:input] = o }
+  opt.on('-s', '--scan <WWISE_DIR>') { |o| options[:scan] = o }
+  opt.on('-m', '--midi <MIDI_DIR') { |o| options[:midi] = o }
+  opt.on('-h', '--help') { puts USAGE; exit }
+end.parse!
+
+if options[:input].nil?
+  puts "Missing input file or directory"
+  puts USAGE
+  exit 1
+end
 
 #fn = File.expand_path("~/code/ntw/fcam/data/bf3/aoc4/chr/ch/ch41141000_wp44_talent00.aab")
 
-input = ARGV[0]
-should_scan = ARGV[1] == "--scan"
+input = options[:input]
+scan_dir = options[:scan]
+midi_dir = options[:midi]
+
 names = []
 
 if input.end_with?(".aab")
@@ -19,6 +68,8 @@ end
 
 files.each do |fn|
   bin = File.read(fn).force_encoding("ASCII-8BIT")
+
+  aab_name = File.basename(fn, ".aab")
 
   i = 4
 
@@ -50,7 +101,9 @@ files.each do |fn|
     puts "SND count: #{sndcount}"
     i += 0x4
 
+    events_timeline = []
     sndcount.times do |sndi|
+
 
       event_hash = bin[i, 4].unpack("V")[0]
 
@@ -61,8 +114,8 @@ files.each do |fn|
       print "SND #{sndi} -- 0x%X -- #{sndname}" % event_hash
       i += 0x40
 
-      f1 = bin[i, 4].unpack("f")[0]
-      print " -- Time #{f1}"
+      ts = bin[i, 4].unpack("f")[0]
+      print " -- Time #{ts}"
       i += 0x4
       u32 = bin[i, 4].unpack("V")[0]
       # if u32 != 0x20
@@ -96,18 +149,65 @@ files.each do |fn|
       i += 0xD0
 
       names << event_name
+      events_timeline << [ts, event_name, sndi]
+      wwise_scan_dir scan_dir, event_name if scan_dir
+    end
 
-      wwise_scan_dir File.expand_path("~/code/ntw/fcam/tools/pck"), event_name if should_scan
+    if midi_dir
+      FileUtils.mkdir_p(midi_dir) unless File.exist?(midi_dir)
+      midi_name = "#{aab_name}_#{sections}"
+      midi_fn = File.join(midi_dir, midi_name + ".mid")
+      File.open(midi_fn, "w") do |f|
+        ppqn = 480
+        fps = 30.0
+        tempo = 150.0
 
-      # event_hash_name = "evt:/#{event_name}"
-      # mur_event_hash = MurmurHash3::V32.str_digest(event_hash_name).unpack("V")[0]
-      # fnv_event_hash = fnvhash32(event_hash_name)
-      # if mur_event_hash != event_hash
-      #   puts "Event hash mismatch: 0x%X != 0x%X" % [mur_event_hash, event_hash]
-      # end
-      # if fnv_event_hash != event_hash
-      #   puts "Event hash mismatch: 0x%X != 0x%X" % [fnv_event_hash, event_hash]
-      # end
+        f.write "MThd"
+        f.write [6].pack("N") # header size
+        f.write [0].pack("n") # format 0
+        f.write [1].pack("n") # 1 track
+        f.write [ppqn].pack("n")
+
+        f.write "MTrk"
+        track_size_pos = f.tell
+        f.write [0].pack("N") # track size placeholder
+
+        f.write midi_var_len_pack(0)
+        f.write [0xFF, 0x51, 0x03].pack("C*") # tempo meta-event
+        micros_per_qn = 60_000_000.0 / tempo
+        f.write u24_pack_be(micros_per_qn)
+
+        micros_per_frame = 1_000_000.0 / fps
+        ticks_per_frame = (micros_per_frame / micros_per_qn) * ppqn
+
+        base_note = 0x3C
+        last_note = nil
+        last_ts = 0
+
+        events_timeline.sort_by { |e| e[0] }.each do |(ts, _, sndi)|
+          delta_ticks = (ts - last_ts) * ticks_per_frame
+          note = base_note + sndi
+          f.write midi_var_len_pack(delta_ticks)
+          if last_note
+            f.write [0x80, last_note, 0x7F].pack("C*")
+            f.write midi_var_len_pack(0)
+          end
+          f.write [0x90, note, 0x7F].pack("C*")
+          last_note = note
+          last_ts = ts
+        end
+
+        f.write midi_var_len_pack(480)
+        if last_note
+          f.write [0x80, last_note, 0x7F].pack("C*")
+          f.write midi_var_len_pack(0)
+        end
+        f.write [0xFF, 0x2F, 0x00].pack("C*")
+
+        track_size = f.tell - track_size_pos - 4
+        f.seek track_size_pos
+        f.write [track_size].pack("N")
+      end
     end
   end
 
